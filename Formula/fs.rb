ee3e0cc40ad0029f7492cbf4f65e44572548210a
Matt Guest <mjcguest@gmail.com>
2025-09-26 12:22:37 +0100

    ready for v2.2.0

--- Formula/fs.rb
+++ Formula/fs.rb
@@ -1,8 +1,8 @@
 class Fs < Formula
   desc "Tyler File Search"
   homepage "https://github.com/ki-aura/fs"
-  url "https://github.com/ki-aura/fs/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "c77da28b7ac9239fe93e23de9565aff2a69a5c2e051497fc6f5400857b9d18d4"
+  url "https://github.com/ki-aura/fs/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "9c321f4e25a4b3f3610efff840b8d60d9e3632754816ea59fbcf28eadb9781a1"
   license "MIT"
 
   def install
