a24b1b7493114f94bfbd441fd640d38f9a097658
Matt Guest <mjcguest@gmail.com>
2025-10-17 12:59:54 +0100

    Rename formula from fs to ggrep and update source references.

--- Formula/ggrep.rb
+++ Formula/ggrep.rb
@@ -1,17 +1,17 @@
-class Fs < Formula
+class GGrep < Formula
   desc "Tyler File Search"
-  homepage "https://github.com/ki-aura/fs"
-  url "https://github.com/ki-aura/fs/archive/refs/tags/v2.5.0.tar.gz"
+  homepage "https://github.com/ki-aura/ggrep"
+  url "https://github.com/ki-aura/ggrep/archive/refs/tags/v2.5.0.tar.gz"
   sha256 "6604d3ae908d216ca8808c7a17fcab7646f7cd825605048d32ec3686e1030d1e"
   license "MIT"
 
   def install
     system "make", "release"
-    bin.install "fs"
+    bin.install "ggrep"
   end
 
   test do
     # Check basic execution
-    system "#{bin}/fs", "-h"
+    system "#{bin}/ggrep", "-h"
   end
 end
