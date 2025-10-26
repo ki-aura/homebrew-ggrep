7856f873071fab20a077461f895e83f3ad53e19e
Matt Guest <mjcguest@gmail.com>
2025-10-26 21:01:17 +0000

    ready for v2.6.3

--- Formula/ggrep.rb
+++ Formula/ggrep.rb
@@ -1,8 +1,8 @@
 class Ggrep < Formula
   desc "Tyler File Search"
   homepage "https://github.com/ki-aura/ggrep"
-  url "https://github.com/ki-aura/ggrep/archive/refs/tags/v2.6.2.tar.gz"
-  sha256 "10a837b6e04d79593ada9ea69b4532e120fcc3c23a919d93c94e756a0709ce73"
+  url "https://github.com/ki-aura/ggrep/archive/refs/tags/v2.6.3.tar.gz"
+  sha256 "10b8829a75cd2308c32da7892372b573b48cc23e73ef39385472d6f02382bfa2"
   license "MIT"
 
   def install
